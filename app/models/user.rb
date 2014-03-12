ad88b66f1e827e207d730509823a543952110e76
Anil Bridgpal <bridgpal@gmail.com>
2014-03-12 15:40:19 +0800

    Add bcrypt and password save.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,25 @@
+require 'bcrypt'
+
 class User
+
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  attr_accessor :password, :password_confirmation
+
   field :email, type: String
+  field :salt, type: String
+  field :fish, type: String
+
+  before_save :encrypt_password
+
+  protected
+
+    def encrypt_password
+      if password.present?
+        self.salt = BCrypt::Engine.generate_salt
+        self.fish = BCrypt::Engine.hash_secret(password, self.salt)
+      end
+    end
 
 end
