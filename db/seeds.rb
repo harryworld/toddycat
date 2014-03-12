5127542cd8c2d8bc350a26b7d89e0ccc8b2c6d9b
Anil Bridgpal <bridgpal@gmail.com>
2014-03-12 13:53:49 +0800

    Add seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.destroy_all
+
+User.create([
+  { email: "anil@ga.co"},
+  { email: "charles@munat.com"},
+  { email: "harry@ga.co"}
+  ])
