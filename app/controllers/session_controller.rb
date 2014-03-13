19098212dcea72a81fdd4598f7a7a293f1ff9db0
Anil Bridgpal <bridgpal@gmail.com>
2014-03-13 10:38:13 +0800

    Add session controller

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,14 @@
+class SessionController < ApplicationController
+
+  def new
+    render text: "Display the log in form."
+  end
+
+  def create
+    render text: "Log the user in."
+  end
+
+  def destroy
+    render text: "Log the user out."
+  end
+end
