3aef09fa55fd3363eaef55280ad3afd010167b84
Anil Bridgpal <bridgpal@gmail.com>
2014-03-13 17:25:56 +0800

    Add create session.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -9,7 +9,15 @@ class SessionController < ApplicationController
   def create
     # render json: params
     # render text: "Log   #{params[:user][:email]} in with #{params[:user][:password]}."
-    render text: User.authenticate(params[:user][:email], params[:user][:password])
+    @user = User.authenticate(params[:user][:email], params[:user][:password])
+
+    if @user
+      session[:user_id] = @user.id
+      render text: "Logged in yo! #{@user.email}"
+    else
+      render text: "Who are you?"
+    end
+
   end
 
   def destroy
