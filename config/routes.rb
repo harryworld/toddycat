cc26130604e4c1858e2d379d460c5f0f54e983a8
Anil Bridgpal <bridgpal@gmail.com>
2014-03-13 10:27:59 +0800

    Add login/logout routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Toddycat::Application.routes.draw do
 
   root 'site#index'
 
+  get 'login' => 'session#new'
+  post 'login' => 'session#create'
+  delete 'logout' => 'session#destroy'
+
+  get 'logout' => 'session#destroy' #TODO: remove before deployment
+
   get 'privacy' => 'site#privacy'
   get 'terms' => 'site#terms'
 
