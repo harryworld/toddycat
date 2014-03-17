03c72d95fddac6afe662554bbd068a0a0e80e10b
Anil Bridgpal <bridgpal@gmail.com>
2014-03-17 11:56:13 +0800

    Add User Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,13 @@ class User
 
   attr_accessor :password, :password_confirmation
 
+
   field :email, type: String
   field :salt, type: String
   field :fish, type: String
 
-  before_save :encrypt_password
+  before_save :set_random_password, :encrypt_password
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
 
   def self.authenticate email, password
     user = User.find_by email: email
@@ -23,6 +25,13 @@ class User
   end
 
   protected
+
+    def set_random_password
+      if self.fish.blank? and password.blank?
+        self.salt = BCrypt::Engine.generate_salt
+        self.fish = BCrypt::Engine.hash_secret(SecureRandom.base64(32), self.salt)
+      end
+    end
 
     def encrypt_password
       if password.present?
