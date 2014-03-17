7a212ccb482119e60433d03a6a2b1c32722d481b
Anil Bridgpal <bridgpal@gmail.com>
2014-03-17 14:46:33 +0800

    Added password reset on User model and SessionController

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -15,7 +15,9 @@ class SessionController < ApplicationController
     password = params[:user][:password]
 
     if password.blank?
-      render text: "Time to reset password" #for any account, valid or not
+      user.set_password_reset if user
+      flash.now[:notice] = "We'll send you an email.."
+      render :new
     elsif user and user.authenticate(password)
       session[:user_id] = user.id
       redirect_to root_url
