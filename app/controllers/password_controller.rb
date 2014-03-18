100aca324d21a292e10740f8b4798bcfd35b9b5e
Anil Bridgpal <bridgpal@gmail.com>
2014-03-18 11:23:46 +0800

    find_by_code update

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -2,17 +2,26 @@
 
   def edit
     @user = User.find_by code: params[:code]
+    redirect_to login_url, notice: "Your reset link has expired, Please generate a new one" unless @user
   end
 
   def update
-    @user = User.find_by code: params[:code]
+    @user = User.find_by_code params[:code]
 
-    if params[:user][:password].blank?
-      @user.errors.add(:password, "This field can't be blank")
-    elsif @user.update_attributes user_params
-      render text: "Success!" and return
+    if @user
+    # if user is found
+      if params[:user][:password].blank?
+        @user.errors.add(:password, "This field can't be blank")
+      elsif @user.update_attributes user_params
+        render text: "Success!"
+      else
+        render text: "Failure!"
+      end
+
+    # otherwise show a message not found
+    else
+      render text: "No code found"
     end
-    render text: @user.errors.messages
 
   end
 
