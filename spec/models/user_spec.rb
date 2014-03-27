6e9a3cee8f2533567cf6ed24695705c1e3e64545
Harry Ng <harryworld@gmail.com>
2014-03-27 15:29:42 +0800

    Add some test cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe User do
-  it "is invalid without an email"
+  it "is valid with an email" do
+    user = User.new(
+      email: 'harry@ga.co'
+    )
+    expect(user).to be_valid
+    # expect(true).to be_true
+  end
+
+  it "is invalid without an email" do
+    user = User.new
+    expect(user).to be_invalid
+  end
+
+  it "is invalid without a salt"
+
+  it "is invalid without a fish"
+
 end
